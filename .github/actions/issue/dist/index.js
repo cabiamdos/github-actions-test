3fc7536b01449a5b6f30029b19266674d9691db3
william <cabiam@criptext.com>
2024-03-31 17:12:00 +0200

    feat: created new issue action

---
+++ .github/actions/issue/dist/index.js
@@ -0,0 +1,90 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 396:
+/***/ ((module) => {
+
+module.exports = eval("require")("@actions/core");
+
+
+/***/ }),
+
+/***/ 716:
+/***/ ((module) => {
+
+module.exports = eval("require")("@actions/github");
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __nccwpck_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			__webpack_modules__[moduleId](module, module.exports, __nccwpck_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete __webpack_module_cache__[moduleId];
+/******/ 		}
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+const core = __nccwpck_require__(396)
+const github = __nccwpck_require__(716)
+
+try {
+  const token = core.getInput('token')
+  const title = core.getInput('title')
+  const body = core.getInput('body')
+  const assignees = core.getInput('assignees')
+
+  const octokit = new github.GitHub(token);
+
+  const response = octokit.issues.create({
+    // owner: github.context.repo.owners
+    // repo: github.context.repo.repo
+    ...github.context.repo,
+    title,
+    body,
+    assignees: assignees ? assignees.split('\n') : undefined
+  })
+
+  core.setOutput('issue', JSON.stringify(response.data))
+
+} catch(err) {
+  core.setFailed(err.message)
+}
+})();
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
