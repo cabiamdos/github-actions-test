53115418481bb59ae7e2e9bd2d4ef498c9aa3877
william <cabiam@criptext.com>
2024-03-31 16:08:17 +0200

    feat: added new core functionality to our action

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -64,13 +64,23 @@ const github = __nccwpck_require__(716)
 
 try {
   // throw(new Error('some error message'))
+
+  core.warning('Warning message')
+  core.error('error message')
+
   const name = core.getInput('who-to-greet')
+  core.setSecret(name);
   console.log(`Hello ${name}`);
   
   const time = new Date()
   core.setOutput('time', time.toTimeString())
   
+  core.startGroup("logging github object")
   console.log(JSON.stringify(github, null, '\t'))
+  core.endGroup()
+
+  core.exportVariable("HELLO", "hello")
+
 } catch(err) {
   core.setFailed(err.message)
 }
--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -4,13 +4,23 @@ const github = require('@actions/github')
 
 try {
   // throw(new Error('some error message'))
+
+  core.warning('Warning message')
+  core.error('error message')
+
   const name = core.getInput('who-to-greet')
+  core.setSecret(name);
   console.log(`Hello ${name}`);
   
   const time = new Date()
   core.setOutput('time', time.toTimeString())
   
+  core.startGroup("logging github object")
   console.log(JSON.stringify(github, null, '\t'))
+  core.endGroup()
+
+  core.exportVariable("HELLO", "hello")
+
 } catch(err) {
   core.setFailed(err.message)
 }
