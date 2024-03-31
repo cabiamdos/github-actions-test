2b149e6ed3f867c8503a1f4f4924acec3d22fd42
william <cabiam@criptext.com>
2024-03-31 20:42:47 +0200

    feat: sending slack messages with php

---
+++ .github/actions/slack-php/script.php
@@ -0,0 +1,69 @@
+<?php
+
+require_once 'vendor/autoload.php';
+Requests::register_autoloader();
+
+// Define the path to your JSON file
+$jsonFile = 'data.json';
+
+// Read the JSON file
+$jsonData = file_get_contents($jsonFile);
+
+// Decode the JSON data into an associative array
+$data = json_decode($jsonData, true);
+
+// var_dump($argv)
+// var_dump($_ENV)
+
+$response = Requests::post(
+  "https://hooks.slack.com/services/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",
+  array(
+    'Content-Type' => 'application/json'
+  ),
+  json_encode(array (
+    'blocks' => 
+        array (
+            array (
+                "type" => "section",
+                "text" => array (
+                    "type" => "mrkdwn",
+                    "text" => $_ENV['INPUT_MESSAGE'],
+                ),
+            ),
+            array (
+                "type" => "section",
+                "fields" => array (
+                    array (
+                        "type" => "mrkdwn",
+                        "text" => "*Repository:*\n{$_ENV['GITHUB_REPOSITORY']}",
+                    ),
+                    array (
+                        "type" => "mrkdwn",
+                        "text" => "*Event:*\n{$_ENV['GITHUB_EVENT_NAME']}",
+                    ),
+                    array (
+                        "type" => "mrkdwn",
+                        "text" => "*Ref:*\n{$_ENV['GITHUB_REF']}",
+                    ),
+                    array (
+                        "type" => "mrkdwn",
+                        "text" => "*SHA:*\n{$_ENV['GITHUB_SHA']}",
+                    ),
+                ),
+            ),
+        ),
+  ))
+  // json_encode(array(
+  //   'text' => 'some message'
+  // ))
+  // json_encode(array($jsonData))
+);
+
+var_dump($response);
+
+if(!$resopnse->success) {
+  // echo 'error'
+  echo $response->body;
+}
+
+?>
---
+++ react-app/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '12544c6db7e9ce6940fa362f0857257b79b31a7f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '12544c6db7e9ce6940fa362f0857257b79b31a7f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'rmccue/requests' => array(
+            'pretty_version' => 'v2.0.11',
+            'version' => '2.0.11.0',
+            'reference' => '31435a468e2357e68df743f2527bda32556a0818',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../rmccue/requests',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
