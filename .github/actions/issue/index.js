12544c6db7e9ce6940fa362f0857257b79b31a7f
william <cabiam@criptext.com>
2024-03-31 17:14:06 +0200

    fix: wrap into async await the issue action

--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -1,25 +1,28 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
+const core = require("@actions/core");
+const github = require("@actions/github");
 
-try {
-  const token = core.getInput('token')
-  const title = core.getInput('title')
-  const body = core.getInput('body')
-  const assignees = core.getInput('assignees')
+async function run() {
+    try {
+        const token = core.getInput("token");
+        const title = core.getInput("title");
+        const body = core.getInput("body");
+        const assignees = core.getInput("assignees");
 
-  const octokit = new github.GitHub(token);
+        const octokit = new github.GitHub(token);
 
-  const response = octokit.issues.create({
-    // owner: github.context.repo.owners
-    // repo: github.context.repo.repo
-    ...github.context.repo,
-    title,
-    body,
-    assignees: assignees ? assignees.split('\n') : undefined
-  })
+        const response = await octokit.issues.create({
+            // owner: github.context.repo.owners
+            // repo: github.context.repo.repo
+            ...github.context.repo,
+            title,
+            body,
+            assignees: assignees ? assignees.split("\n") : undefined
+        });
 
-  core.setOutput('issue', JSON.stringify(response.data))
-
-} catch(err) {
-  core.setFailed(err.message)
+        core.setOutput("issue", JSON.stringify(response.data));
+    } catch (err) {
+        core.setFailed(err.message);
+    }
 }
+
+run();
